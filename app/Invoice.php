33d3cde4a880b8e763be8e78c2c9c198bda3db29
Valeria Granada Rodas <55161707+vale0722@users.noreply.github.com>
2020-04-22 18:39:47 -0500

    relationship between models

--- app/Invoice.php
+++ app/Invoice.php
@@ -6,5 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice extends Model
 {
-    //
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class)->withPivot(['quantity', 'unit_value, total_value']);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    public function getSubtotalAttribute()
+    {
+        if (isset($this->products[0])) {
+            return $this->products[0]->pivot->where('invoice_id', $this->id)->sum('total_value');
+        }
+        return 0;
+    }
+
+    public function getVatAttribute()
+    {
+        $subtotal = $this->subtotal;
+        $vat = $this->vat;
+        return $subtotal + $vat;
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function invoices()
+    {
+        return $this->belongsToMany(Invoice::class);
+    }
 }
